ea469c0b63ace27c59427eb343ca2b009b922871
VladVegera <94718115+VladVegera@users.noreply.github.com>
2022-06-10 21:01:45 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,63 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+class BST {
+ private:
+  struct Node {
+    T value;
+    int count;
+    Node *lef;
+    Node *rig;
+  };
+  Node *root;
+  Node* addNode(Node *root, T v) {
+    if (!root) {
+      root = new Node;
+      root->value = v;
+      root->left = root->rig = nullptr;
+    } else if (root->value > v) {
+      root->left = addNode(root->left, v);
+    } else if (root->value < v) {
+      root->rig = addNode(root->rig, v);
+    } else {
+      (root->count)++;
+    }
+    return root;
+  }
+  int heightTree(Node* root) {
+    if (!root) {
+      return 0;
+    }
+    int Left = heightTree(root->left);
+    int Right = heightTree(root->rig);
+    if (Left > Right) {
+      return (Left + 1);
+    } else {
+      return (Right + 1);
+    }
+  }
+  int searchValue(Node* root, T v) {
+    if (!root) {
+      return 0;
+    } else if (root->value == v) {
+      return root->count;
+    } else if (root->value > v) {
+      return searchValue(root->left, v);
+    } else {
+      return searchValue(root->rig, v);
+    }
+  }
+  
+  public:
+    BST() :root(nullptr) {}
+    void add(T v) {
+      root = addNode(root, v);
+    }
+    int search(T v) {
+      return searchValue(root, v) + 1;
+    }
+    int depth() {
+      return heightTree(root) - 1;
+    }
+};
 #endif  // INCLUDE_BST_H_
